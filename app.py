2335fd39c1d023629fe8831e55c48d675103600c
Ijasgit <mohammedijasn3899@gmail.com>
2022-09-02 16:26:58 +0530

    .

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ def home():
     return render_template("index.html", datas=data)
 
 
-@app.route("/add", methods=["GET", "POST"])
+
+@app.route("/add_user", methods=["GET", "POST"])
 def add_user():
     if request.method == "POST":
         s_name = request.form["name"]
@@ -26,28 +27,30 @@ def add_user():
         cur.execute("insert into students (NAME,AGE,LANGUAGE,PREFERENCE) values(?,?,?,?)",
                     (s_name, s_age, s_language, s_preference))
         conn.commit()
-        return redirect(url_for('home'))
+        return redirect(url_for("home"))
     return render_template("add_user.html")
 
 
-# @app.route("/edit_user/<string:id>", methods=["GET", "POST"])
-# def edit_user(id):
-#     if request.method == "POST":
-#         s_name = request.form["name"]
-#         s_age = request.form["age"]
-#         s_language = request.form["language"]
-#         s_preference = request.form["preference"]
-#         conn=sql.connect("crud2.db") #doubt
-#         cur=conn.cursor()
-#         cur.execute("update students set NAME=?,AGE=?,LANGUAGE=?,PREFERENCE=? where ID=?",(s_name,s_age,s_language,s_preference,id))
-#         conn.commit()
-#         return redirect(url_for('home'))
-#     conn = sql.connect('crud2.db')
-#     conn.row_factory = sql.Row
-#     cur = conn.cursor()
-#     cur.execute("select * from students where ID=?",(id,))
-#     data=cur.fetchone()
-#     return render_template("edit_user.html",datas=data)
+@app.route("/edit_user/<string:id>", methods=["POST","GET"])
+def edit_user(id):
+    if request.method == "POST":
+        s_name = request.form["name"]
+        s_age = request.form["age"]
+        s_language = request.form["language"]
+        s_preference = request.form["preference"]
+        conn=sql.connect("crud2.db") #doubt
+        cur=conn.cursor()
+        cur.execute("update students set NAME=?,AGE=?,LANGUAGE=?,PREFERENCE=? where ID=?",(s_name,s_age,s_language,s_preference,id))
+        conn.commit()
+        return redirect(url_for('home'))
+    conn = sql.connect('crud2.db')
+    conn.row_factory = sql.Row
+    cur = conn.cursor()
+    cur.execute("select * from students where ID=?",(id,))
+    data=cur.fetchone()
+    return render_template("edit_user.html",datas=data)
+
+
 
 
 if __name__ == "__main__":
